df0c33f1bdb6e70eb544061fe1ae29db43cd9a50
Yashraj Talokar <70269192+yashrajtalokar@users.noreply.github.com>
2022-12-06 23:25:39 +0530

    Files added

---
+++ src/BackgroundAnimate.js
@@ -0,0 +1,23 @@
+const BackgroundAnimate = () => {
+  return (
+        <ul className="background">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+  )
+}
+
+export default BackgroundAnimate
